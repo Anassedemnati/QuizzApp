ecd5e5d0a403f2584bc6c732589907c3ee243a2a
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2021-11-26 00:33:43 +0100

    state less RaisedeButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
-
 import 'package:flutter/material.dart';
+
 // void main(){
 //   runApp(MyApp());
 // }
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
-
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My First app'),),
-      body: Text('this is my body text'),
-    ),);
+class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  void answearQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
-  
+
+  @override
+  Widget build(BuildContext context) {
+    var question = [
+      'what\'s your favourit color?',
+      'what\'s your favourit color?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First app'),
+        ),
+        body: Column(
+          children: [
+            Text(question[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answearQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('Answer 3 chosen!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer 4'),
+              onPressed: answearQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
